2ac89946f9946491a115a291882ecd70764fd5a9
Guilherme <guilherme.oliveira@magazineluiza.com.br>
2024-11-12 14:23:06 +0000

    classificacao

--- core/urls.py
+++ core/urls.py
@@ -5,12 +5,12 @@ from app_Disparo_de_DAGs.urls import *
 #importação app quadro de metas
 from quadro_de_metas.urls import *
 #importacao app de classificacao
-from classificacao.urls import *
+from classificacao2.urls import *
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('app_Disparo_de_DAGs.urls')),
     path('api/v1/', include('quadro_de_metas.urls')),
-    path('api/v1/', include('classificacao.urls')),
+    path('api/v1/', include('classificacao2.urls')),
 ]
