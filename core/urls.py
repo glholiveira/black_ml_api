2fe8df0a7d1a7bd548539764421e7c61fc0a5842
Guilherme <guilherme.oliveira@magazineluiza.com.br>
2024-11-12 13:02:53 +0000

    atualizacao

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,13 @@ from django.urls import path, include
 from app_Disparo_de_DAGs.urls import *
 #importação app quadro de metas
 from quadro_de_metas.urls import *
+#importacao app de classificacao
+from classificacao.urls import *
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('app_Disparo_de_DAGs.urls')),
     path('api/v1/', include('quadro_de_metas.urls')),
+    path('api/v1/', include('classificacao.urls')),
 ]
